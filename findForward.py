6782677e67559ec57f9147e583bd2ae899a1790b
misha <stun007@gmail.com>
2017-08-01 19:03:05 +0300

    +

--- findForward.py
+++ findForward.py
@@ -1,22 +1,33 @@
 import os
 
 
-def for_file(result=[], file=os.DirEntry(), search_words=[]):
+def for_file(file, result=[], search_words=[]):
     if file.is_file():
-        with open(file.path(), 'r') as f:
+        with open(file.path, 'r') as f:
             lines = f.readlines()
             for line in lines:
                 for word in search_words:
                     if word in line:
-                        result.append(line)
+                        result.append(line.replace("\n", ""))
     else:
         print("Пришла папка")
     return result
 
 
-if __name__ == '__main__':
-    pathDir = "C:\\Users\\Tom\\Desktop\\1"  # input("Введите путь:")
+def f():
+    result = []
     stack = [os.scandir(pathDir)]
-    while True:
+    while stack:
         for i in stack.pop():
-            i = os.DirEntry(i)
+            if i.is_dir():
+                stack.append(os.scandir(i.path))
+            if i.is_file():
+                result = for_file(i, result=result, search_words=words)
+    return result
+
+if __name__ == '__main__':
+    pathDir = input("Введите путь:\n")
+    words = input("Слова для поиска:\n").split()
+
+    for i in f():
+        print(i)
