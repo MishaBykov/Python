81a36d76aaac5eef6484e78a41dd16fd04d06390
misha <stun007@gmail.com>
2017-09-18 09:59:38 +0300

    +

--- findForward.py
+++ findForward.py
@@ -15,28 +15,33 @@ def to_file(path_file):
             return 6
         return 0
 
-    def i_d_2():
-        pass
+    def i_d_2(string):
+        if ";" in string:
+            return 0
 
-    def i_d_3():
+    def i_d_3(string):
         pass
 
     def i_d_4():
         pass
 
-    def i_d_5():
+    def i_d_5(string):
         pass
 
+    def i_d_6(string):
+        pass
 
     brackets_depth = 0
     i_d = 0
     active_string = ""
-    with open(path_file, 'r') as p_f:
+    path_ns = ""
+    funcs = [i_d_0, i_d_1, i_d_2, i_d_3, i_d_4, i_d_5]
+    with open(path_file, 'r') as o_f:
         pass
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Replace of part of a name')
+    parser = argparse.ArgumentParser(description='find forward')
     parser.add_argument("pathInput", type=str,
                         help="input path dir")
     parser.add_argument("pathOutput", type=str,
