1baeb5155bf6b55243b1695e427209167e028dba
misha <stun007@gmail.com>
2017-09-25 15:08:32 +0300

    +

--- findForward.py
+++ findForward.py
@@ -2,40 +2,50 @@ import argparse
 
 
 def to_file(path_file):
-    def i_d_0(string):
+    def next_str(file):
+        global brackets_depth
+        st = str(file.readline())
+        brackets_depth += st.count("{") - st.count("}")
+        return st
+
+    def id_0(string):
         if "const" in string:
             return 1
-        if "class" in string or "struct" in string:
+        elif "class" in string or "struct" in string:
             return 2
-        if "typedef" in string:
+        elif "typedef" in string:
             return 3
+        elif "enum" in string:
+            return 4
+        elif "namespace" in string:
+            return 5
+        return 0
 
-    def i_d_1(string):
+    def id_1(string):
         if not ("(" in string) or string.find("=") < string.find("("):
             return 6
         return 0
 
-    def i_d_2(string):
+    def id_2(string):
         if ";" in string:
             return 0
 
-    def i_d_3(string):
+    def id_3(string):
         pass
 
-    def i_d_4():
+    def id_4():
         pass
 
-    def i_d_5(string):
+    def id_5(string):
         pass
 
-    def i_d_6(string):
+    def id_6(string):
         pass
 
     brackets_depth = 0
-    i_d = 0
-    active_string = ""
-    path_ns = ""
-    funcs = [i_d_0, i_d_1, i_d_2, i_d_3, i_d_4, i_d_5]
+    string = ""
+    ns = ns_root
+    funcs = [id_0, id_1, id_2, id_3, id_4, id_5]
     with open(path_file, 'r') as o_f:
         pass
 
@@ -50,7 +60,7 @@ if __name__ == '__main__':
     source = args.pathInput
     out = args.pathOutput
     fOut = open(out, 'a')
-    ns = [{}, []]
+    ns_root = [{}, [], 0]
     with open(source, 'r') as s:
         for line in s:
             to_file(line)
