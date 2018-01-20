e08db7d7845dab883bd88c0f377313d455b21247
Misha <stun007@gmail,com>
2018-01-20 13:13:51 +0300

    +

--- dirs/un_dirs.py
+++ dirs/un_dirs.py
@@ -16,7 +16,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Replace of part of a name')
     parser.add_argument("path", type=str, help="input path dir")
     args = parser.parse_args()
-    files = list(os.scandir(args.path))
     for file in os.scandir(args.path):
         if file.is_dir():
             move_all_from_dir(file.path, args.path)
