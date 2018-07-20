e1e14c153109e0dcecadde2c7e95e26a48a13013
md.bykov <md.bykov@tensor.ru>
2018-07-20 18:15:00 +0300

    +

--- autoReport.py
+++ autoReport.py
@@ -1,9 +1,16 @@
-class Report():
+class Report:
     def __init__(self, email):
         self.email = email
 
     def send(self):
         pass
+
+    def generate(self):
+        pass
+
+    def save_email(self, file_name):
+        with open(file_name, "w") as email_file:
+            email_file.write(self.email+'\n')
 
 
 class Task():
