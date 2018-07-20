35525bc0d18c7afeca14b4326139b44c8c104550
misha <stun007@gmail.com>
2018-07-20 11:25:53 +0300

    замена на классы

--- autoReport.py
+++ autoReport.py
@@ -1,10 +1,24 @@
-def send_report():
-    pass
+class Report():
+    def __init__(self):
+        pass
+
+    def send(self):
+        pass
+
+    def setup_email(self):
+        pass
 
 
-def begin_task():
-    pass
+class Task():
+    def __init__(self):
+        pass
+
+    def begin_task(self):
+        pass
+
+    def end_task(self):
+        pass
 
 
-def end_task():
-    pass
+
+
