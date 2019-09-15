9bed600dc42151ca8d81cc9bb78f7ec14c8a3402
misha <mike008@bk.ru>
2019-09-15 23:07:42 +0300

    +

--- to_dirs.py
+++ to_dirs.py
@@ -1,48 +1,39 @@
 import argparse
 import shutil as sh
+import rename as rn
 import os
-
-
-def new_name(old_name, shift_name, length_name):
-    return ((length_name - len(str(old_name + shift_name))) * "0") + str(old_name + shift_name)
 
 
 def sort_alphabet(input_str):
     return str(input_str)[0].lower()
 
 
-def f(files, count_one_dir, name_dir, shift_name, length_name):
-    try:
-        for i in range(len(files) // count_one_dir + (0 if 0 == len(files) % count_one_dir else 1)):
-            name = new_name(name_dir, shift_name, length_name)
-            os.mkdir(path + '\\' + name)
-            for file in files[i * count_one_dir:(i + 1) * count_one_dir]:
-                sh.move(path + '\\' + file, path + '\\' + name)
-            name_dir += 1
-    except sh.Error:
-        f(files, count_one_dir, name_dir, shift_name, length_name + 1)
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Replace of part of a name')
     parser.add_argument("path", type=str,
                         help="input path dir")
-    parser.add_argument("-ns", "--nameStart", nargs='?',
-                        help="input nameStart")
-    parser.add_argument("-cod", "--countOneDir", nargs='?',
+    parser.add_argument("-cod", "--count_one_dir", nargs='?',
                         help="input countOneDir ")
     args = parser.parse_args()
     path = args.path
-    countOneDir = args.countOneDir
-    if countOneDir is None:
-        countOneDir = int(input("Количество файлов на одну папку: "))
-    nameStart = args.nameStart
-    if nameStart is None:
-        nameStart = int(input("nameStart: "))
-    shiftName = nameStart
-
+    if args.count_one_dir is None:
+        count_one_dir = input("Количество файлов на одну папку: ")
+    else:
+        count_one_dir = args.count_one_dir
+    count_one_dir = int(count_one_dir)
     files_list = sorted(os.listdir(path), key=sort_alphabet)
+    count_files = len(files_list)
     os.chdir(path)
-    lengthName = int(len(str(len(files_list) // countOneDir + shiftName)))
-    nameDir = 0
-    f(files_list, countOneDir, nameDir, shiftName, lengthName)
+    count_new_dir = count_files // count_one_dir + 1
+    length_name = int(len(str(count_new_dir)))
+    ind_file = 0
+    for dir_name in range(1, count_new_dir + 1):
+        dir_name = '0' * (length_name - len(str(dir_name))) + str(dir_name)
+        while os.path.exists(dir_name):
+            dir_name = '0' + dir_name
+        os.mkdir(dir_name)
+        for i in range(0, count_one_dir):
+            rn.rename(path, files_list[ind_file], new_path=dir_name)
+            ind_file += 1
+            if ind_file == count_files:
+                break
