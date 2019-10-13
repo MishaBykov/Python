23c23c4d14810140e07a964ec9ad8665f0a8bf77
Misha Bykov <mike008@bk.ru>
2019-10-13 15:52:06 +0300

    + Renamer

--- rename.py
+++ rename.py
@@ -3,31 +3,49 @@ import re
 import shutil as sh
 
 
-def __new_name(old_name, ins):
-    ins = '(' + str(ins) + ')'
-    old_name = old_name.rsplit('.', 1)
-    old_name[0] = old_name[0] + ' ' + ins
-    return str.join('.', old_name)
+class Renamer:
 
+    def __init__(self, path, replace=None, regex=None, new_path=None):
+        self.zeros_name = set()
+        self.path = path
+        self.replace = replace
+        self.regex = regex
+        if self.new_path is None:
+            self.new_path = self.path
+        else:
+            self.new_path = new_path
 
-def rename(path, name, replace=None, regex=None, new_path=None):
-    if new_path is None:
-        new_path = path
-    a = 0
-    new_name = ''
-    if replace:
-        new_name = name.replace(replace[0], replace[1])
-    elif regex:
-        new_name = re.sub(regex[0], regex[1], name)
-    elif new_path:
-        new_name = name
-        # [on_true] if [expression] else [on_false]
-    if os.path.join(path, name) == os.path.join(new_path, new_name):
-        return
-    new_name_ins = os.path.join(new_path, (__new_name(new_name, a)) if a > 0 else new_name)
-    while os.path.exists(new_name_ins):
-        a += 1
-        new_name_ins = os.path.join(new_path, (__new_name(new_name, a)) if a > 0 else new_name)
-    sh.move(os.path.join(path, name), new_name_ins)
-    if a != 0:
-        sh.move(os.path.join(new_path, new_name), os.path.join(new_path, __new_name(new_name, 0)))
+    @staticmethod
+    def __new_name_ind(old_name, ins):
+        ins = '(' + str(ins) + ')'
+        old_name = old_name.rsplit('.', 1)
+        old_name[0] = old_name[0] + ' ' + ins
+        return str.join('.', old_name)
+
+    def __del__(self):
+        for name in self.zeros_name:
+            sh.move(os.path.join(self.new_path, name), os.path.join(self.new_path, Renamer.__new_name_ind(name, 0)))
+
+    def rename(self, name):
+        a = 0
+        new_name = ''
+        if self.replace:
+            new_name = name.replace(self.replace[0], self.replace[1])
+        elif self.regex:
+            new_name = re.sub(self.regex[0], self.regex[1], name)
+        elif self.new_path:
+            new_name = name
+            # [on_true] if [expression] else [on_false]
+        if name == new_name:
+            return
+        path_new_name = os.path.join(self.new_path, new_name)
+        exist_path = os.path.exists(path_new_name)
+        if os.path.exists(exist_path):
+            path_new_name_ins = os.path.join(self.new_path, (Renamer.__new_name_ind(new_name, a)))
+            while os.path.exists(path_new_name_ins):
+                a += 1
+                path_new_name_ins = os.path.join(self.new_path, (Renamer.__new_name_ind(new_name, a)))
+            self.zeros_name.add(path_new_name)
+            sh.move(os.path.join(self.path, name), path_new_name_ins)
+        else:
+            sh.move(os.path.join(self.path, name), path_new_name)
