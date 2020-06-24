d1d31941f1c68c40f1ecb0896f0b8608bacd8816
Misha Bykov <mike008@bk.ru>
2020-06-24 15:54:52 +0300

    update dirs to zip

--- dirs_to_zip.py
+++ dirs_to_zip.py
@@ -1,35 +1,39 @@
+import argparse
 import os
 import zipfile
 
 spaces = "    "
 
-# todo source path to params
-os.chdir(r'')
-dirs = os.scandir()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='from dir dirs to zip')
+    parser.add_argument("source_path_dir", type=str, help="source path dir")
+    parser.add_argument("destination_path_dir", type=str, help="destination path dir")
+    args = parser.parse_args()
 
-# todo destination path to params
-destination = r''
+    os.chdir(args.source_path_dir)
+    dirs = os.scandir()
 
-for d in dirs:
-    d: os.DirEntry
-    if d.is_dir():
-        print(d.name)
-        zip_name = d.name + '.zip'
-        path_destination_zip = os.path.join(destination, zip_name)
-        if os.path.exists(path_destination_zip):
-            if os.path.exists(zip_name):
-                os.remove(zip_name)
-            print(spaces + "exist destination")
-            continue
-        temp_path_zip = os.path.join(destination, "temp.zip")
-        print(spaces + "create zip")
-        z = zipfile.ZipFile(temp_path_zip, 'w')  # Создание нового архива
-        z.namelist()
-        print(spaces + "fill zip")
-        for root, dirs, files in os.walk(d.path):  # Список всех файлов и папок в директории folder
-            for file in files:
-                z.write(os.path.join(root, file))  # Создание относительных путей и запись файлов в архив
-        z.close()
-        print(spaces + "rename")
-        os.rename(temp_path_zip, path_destination_zip)
+    destination = args.destination_path_dir
+
+    for d in dirs:
+        d: os.DirEntry
+        if d.is_dir():
+            print(d.name)
+            zip_name = d.name + '.zip'
+            path_destination_zip = os.path.join(destination, zip_name)
+            if os.path.exists(path_destination_zip):
+                if os.path.exists(zip_name):
+                    os.remove(zip_name)
+                print(spaces + "exist destination")
+                continue
+            temp_path_zip = os.path.join(destination, "temp.zip")
+            print(spaces + "create zip")
+            z = zipfile.ZipFile(temp_path_zip, 'w')  # Создание нового архива
+            print(spaces + "fill zip")
+            for root, dirs, files in os.walk(d.path):  # Список всех файлов и папок в директории folder
+                for file in files:
+                    z.write(os.path.join(root, file))  # Создание относительных путей и запись файлов в архив
+            z.close()
+            print(spaces + "rename")
+            os.rename(temp_path_zip, path_destination_zip)
 
