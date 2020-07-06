3c2602257578e85ed2baa481844050e179b5e38f
misha <mike008@bk.ru>
2020-07-07 01:58:40 +0300

    +

--- Rename.py
+++ Rename.py
@@ -4,51 +4,84 @@ import shutil as sh
 
 
 class Rename:
-    def __init__(self, path, replace=None, regex=None, new_path=None):
-        self.zeros_name = set()
-        self.path = path
-        self.replace = replace
-        self.regex = regex
+    __regex: list
+    __replace: list
+    __source_path: str
+    __new_path: str
+
+    def __init__(self, source_path=None, replace=None, regex=None, new_path=None):
+        self.__zeros_name = set()
+        self.__source_path = source_path
+        self.__replace = replace
+        self.__regex = regex
         if new_path is None:
-            self.new_path = self.path
+            self.__new_path = self.__source_path
         else:
-            self.new_path = new_path
+            self.__new_path = new_path
 
     def __del__(self):
-        while self.zeros_name:
-            name = self.zeros_name.pop()
-            if os.path.exists(os.path.join(self.new_path, name)):
-                self.rename(os.path.join(self.new_path, name),
-                            os.path.join(self.new_path, Rename.__new_name_ind(name, 0)))
-            else:
-                print("not found from [zeros_name]: " + os.path.join(self.new_path, name))
+        self.clear_zeros()
+
+    @property
+    def replace(self):
+        return self.__replace
+
+    @replace.setter
+    def replace(self, value):
+        self.__replace = value
+
+    @property
+    def regex(self):
+        return self.__regex
+
+    @regex.setter
+    def regex(self, value):
+        self.__regex = value
+
+    @property
+    def source_path(self):
+        return self.__source_path
+
+    @source_path.setter
+    def source_path(self, value):
+        self.__source_path = value
+
+    @property
+    def new_path(self):
+        return self.__new_path
+
+    @new_path.setter
+    def new_path(self, value):
+        self.__new_path = value
 
     def get_new_name(self, name: str) -> str:
         new_name = ''
-        if self.replace:
-            new_name = name.replace(self.replace[0], self.replace[1])
-        elif self.regex:
-            new_name = re.sub(self.regex[0], self.regex[1], name)
-        elif self.new_path:
+        if self.__replace:
+            new_name = name.replace(self.__replace[0], self.__replace[1])
+        elif self.__regex:
+            new_name = re.sub(self.__regex[0], self.__regex[1], name)
+        elif self.__new_path:
             new_name = name
         return new_name
 
     def rename(self, name: str, new_name):
-        if os.path.exists(os.path.join(self.new_path, new_name)):
+        new_path_new_name = os.path.join(self.__new_path, new_name)
+        source_path_name = os.path.join(self.__source_path, name)
+        if os.path.exists(new_path_new_name):
             new_name_ins = new_name
-            # todo что здесь происходит?
             a = 0
-            while os.path.exists(os.path.join(self.new_path, new_name_ins)):  # and new_name_ins != name:
+            while os.path.exists(os.path.join(self.__new_path, new_name_ins)):  # and new_name_ins != name:
                 a += 1
                 new_name_ins = Rename.__new_name_ind(new_name, a)
-            self.zeros_name.add(new_name)
-            sh.move(os.path.join(self.path, name), os.path.join(self.new_path, new_name_ins))
+            self.__zeros_name.add(new_path_new_name)
+            sh.move(source_path_name, os.path.join(self.__new_path, new_name_ins))
         else:
-            sh.move(os.path.join(self.path, name), os.path.join(self.new_path, new_name))
+            sh.move(os.path.join(self.__source_path, name), new_path_new_name)
 
     def main(self, name: str):
         new_name = self.get_new_name(name)
-        if name == new_name and self.path == self.new_path:
+        if self.__source_path is None or name == new_name and self.__source_path == self.__new_path:
+            print('skip:name= ' + name)
             return
         self.rename(name, new_name)
 
@@ -57,3 +90,20 @@ class Rename:
         ins = ' ({})'.format(ins)
         name_ext = os.path.splitext(old_name)
         return str.join(ins, name_ext)
+
+    def clear_zeros(self):
+        back_up_source = self.__source_path
+        back_up_new = self.__new_path
+        while self.__zeros_name:
+            path_name = self.__zeros_name.pop()
+            path = os.path.dirname(path_name)
+            self.source_path = path
+            self.new_path = path
+            name = os.path.basename(path_name)
+            if os.path.exists(path_name):
+                self.rename(name, self.__new_name_ind(name, 0))
+            else:
+                print("not found from [zeros_name]: " + path_name)
+        self.source_path = back_up_source
+        self.new_path = back_up_new
+
--- un_dirs.py
+++ un_dirs.py
@@ -2,20 +2,15 @@ import argparse
 from Rename import Rename
 import os
 
-
-def move_all_from_dir(src, dst):
-    rename = Rename(src, new_path=dst)
-    for file in os.listdir(src):
-        rename.main(file)
-    del rename
-
-
-# todo из всез перенести в одну папку, потом из нее
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Replace of part of a name')
     parser.add_argument("path", type=str, help="input path dir")
     args = parser.parse_args()
-    for file in os.scandir(args.path):
-        if file.is_dir():
-            move_all_from_dir(file.path, args.path)
-            os.rmdir(file.path)
+    r = Rename(new_path=args.path)
+    for i in os.scandir(args.path):
+        if i.is_dir():
+            r.source_path = i.path
+            for j in os.listdir(i.path):
+                r.main(j)
+            os.rmdir(i.path)
+    del r
