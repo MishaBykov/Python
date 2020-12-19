6c2505ad9a9e7fec8f4f91a2bb712595aad2aa49
misha <mike008@bk.ru>
2020-12-19 16:10:11 +0300

    add parse_app_manifest

--- steam_lnk.py
+++ steam_lnk.py
@@ -1,14 +1,19 @@
 import os
 
-path_dir = r"test"
-path_ico = r"test\Aragami.url"
-path_exe = r"D:\FilesGames\SteamLib\steamapps\common\BloodCard\BloodCard.exe"
+paths = {
+    "icon_test": r"test_icon\Aragami.url",
+    "exe_test": r"D:\FilesGames\SteamLib\steamapps\common\BloodCard\BloodCard.exe",
+
+    "icon_dirs": r"test_icon",
+    "app_manifests": r"D:\FilesGames\SteamLib\steamapps",
+    "install_games": r"D:\FilesGames\SteamLib\steamapps\common"
+}
 
 
-def parse_data_icon(path: str) -> dict:
+def parse_data_icon(path_icon: str) -> dict:
     result = {}
     key_root = ''
-    with open(path_ico) as file_ico:
+    with open(path_icon) as file_ico:
         for string in file_ico:
             string = string.strip()
             if '=' in string and key_root:
@@ -20,17 +25,54 @@ def parse_data_icon(path: str) -> dict:
     return result
 
 
-def data_icon_to_strings(data_icon: dict, path: str):
+def write_data_icon(data_icon: dict, path_icon: str):
     strings = []
     for key1 in data_icon:
         strings.append('[' + key1 + ']')
         for key2 in data_icon[key1]:
             strings.append(key2 + '=' + data_icon[key1][key2])
-    with open(path_ico, 'w') as ico:
+    with open(path_icon, 'w') as ico:
         ico.write('\n'.join(strings))
 
 
+def parse_app_manifest(path_manifest: str) -> dict:
+    result = {}
+    key_path = []
+    with open(path_manifest) as file:
+        current_dict = result
+        for string in file:
+            string = string.replace('"', '').split()
+            split_len = len(string)
+            if split_len == 1:
+                if string[0] == '}':
+                    key_path.pop()
+                    current_dict = result
+                    for key in key_path:
+                        current_dict = current_dict[key]
+                elif string[0] == '{':
+                    current_dict[key_path[-1]] = {}
+                    current_dict = current_dict[key_path[-1]]
+                else:
+                    key_path.append(string[0])
+            elif split_len == 2:
+                current_dict[string[0]] = string[1]
+
+    return result
+
+
+def get_dict_app_manifests(path_manifests: str) -> dict:
+    result = {}
+    for de in os.scandir(path_manifests):
+        de: os.DirEntry
+
+    return result
+
+
+def main():
+    data_icon = parse_data_icon(paths["icon_test"])
+    data_icon['InternetShortcut']['IconFile'] = paths["exe_test"]
+    write_data_icon(data_icon, paths["icon_test"])
+
+
 if __name__ == '__main__':
-    data_icon = parse_data_icon(path_ico)
-    data_icon['InternetShortcut']['IconFile'] = path_exe
-    data_icon_to_strings(data_icon, path_ico)
+    main()
