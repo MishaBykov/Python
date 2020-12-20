14f5adde3f400344338b281d7fef400a2d580966
misha <mike008@bk.ru>
2020-12-20 20:40:00 +0300

    +

--- steam_lnk.py
+++ steam_lnk.py
@@ -1,33 +1,46 @@
 import os
 
 paths = {
-    "icon_test": r"test_icon\Aragami.url",
-    "exe_test": r"D:\FilesGames\SteamLib\steamapps\common\BloodCard\BloodCard.exe",
-
     "icon_dirs": r"test_icon",
     "app_manifests": r"D:\FilesGames\SteamLib\steamapps",
     "install_games": r"D:\FilesGames\SteamLib\steamapps\common"
 }
 
 
+def list_icons_data(path_dir_icons: str) -> dict:
+    result = {}
+    for file in os.scandir(path_dir_icons):
+        file: os.DirEntry
+        if file.name.rsplit(os.path.extsep, 1)[-1] == "url":
+            data_icon = parse_data_icon(file.path)
+            if data_icon['InternetShortcut']["URL"].split(':')[0] == "steam":
+                game_id = data_icon['InternetShortcut']["URL"].rsplit('/', 1)[-1]
+                result[game_id] = data_icon
+    return result
+
+
 def parse_data_icon(path_icon: str) -> dict:
     result = {}
     key_root = ''
-    with open(path_icon) as file_ico:
-        for string in file_ico:
-            string = string.strip()
-            if '=' in string and key_root:
-                key, value = string.split('=')
-                result[key_root][key] = value
-            if string[0] == '[':
-                key_root = string[string.find('[') + 1:string.find(']')]
-                result[key_root] = {}
+    if os.path.exists(path_icon):
+        result["path_icon"] = path_icon
+        with open(path_icon) as file_ico:
+            for string in file_ico:
+                string = string.strip()
+                if '=' in string and key_root:
+                    key, value = string.split('=', 1)
+                    result[key_root][key] = value
+                if string[0] == '[':
+                    key_root = string[string.find('[') + 1:string.find(']')]
+                    result[key_root] = {}
     return result
 
 
 def write_data_icon(data_icon: dict, path_icon: str):
     strings = []
     for key1 in data_icon:
+        if key1 == "path_icon":
+            continue
         strings.append('[' + key1 + ']')
         for key2 in data_icon[key1]:
             strings.append(key2 + '=' + data_icon[key1][key2])
@@ -56,11 +69,10 @@ def parse_app_manifest(path_manifest: str) -> dict:
                     key_path.append(string[0])
             elif split_len == 2:
                 current_dict[string[0]] = string[1]
-
     return result
 
 
-def get_dict_app_manifests(path_manifests: str) -> dict:
+def get_app_manifests(path_manifests: str) -> dict:
     result = {}
     for de in os.scandir(path_manifests):
         de: os.DirEntry
@@ -86,11 +98,16 @@ def find_fist_exe(path_dir: str) -> str:
 
 
 def main():
-    data_icon = parse_data_icon(paths["icon_test"])
-    data_icon['InternetShortcut']['IconFile'] = paths["exe_test"]
-    write_data_icon(data_icon, paths["icon_test"])
-    r = parse_app_manifest(r"D:\FilesGames\SteamLib\steamapps\appmanifest_3720.acf")
-    path_exe = find_fist_exe(paths["install_games"])
+    icons_data = list_icons_data(paths["icon_dirs"])
+    app_manifests_data = get_app_manifests(paths["app_manifests"])
+    for game_id in icons_data.keys():
+        install_dir = app_manifests_data[game_id]["AppState"]["installdir"]
+        path_fist_exe = find_fist_exe(os.path.join(paths["install_games"], install_dir))
+        icons_data[game_id]["InternetShortcut"]["IconFile"] = path_fist_exe
+        if icons_data[game_id]["InternetShortcut"]["IconIndex"] != '0':
+            icons_data[game_id]["InternetShortcut"]["IconIndex"] = '0'
+        write_data_icon(icons_data[game_id], icons_data[game_id]["path_icon"])
+
 
 if __name__ == '__main__':
     main()
