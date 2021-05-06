c8116c3440e0785390ff47eb28979586f21f5618
misha <mike008@bk.ru>
2021-05-06 21:42:23 +0300

    add todo

--- dirs_to_zip.py
+++ dirs_to_zip.py
@@ -2,8 +2,8 @@ import argparse
 import os
 import zipfile
 
+# todo добавить логирование
 spaces = "    "
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='from dir dirs to zip')
     parser.add_argument("source_path_dir", type=str, help="source path dir")
@@ -24,16 +24,16 @@ if __name__ == '__main__':
             if os.path.exists(path_destination_zip):
                 if os.path.exists(zip_name):
                     os.remove(zip_name)
-                print(spaces + "exist destination")
+                # print(spaces + "exist destination")
                 continue
             temp_path_zip = os.path.join(destination, "temp.zip")
-            print(spaces + "create zip")
+            # print(spaces + "create zip")
             z = zipfile.ZipFile(temp_path_zip, 'w')  # Создание нового архива
-            print(spaces + "fill zip")
+            # print(spaces + "fill zip")
             for root, dirs, files in os.walk(d.path):  # Список всех файлов и папок в директории folder
                 for file in files:
                     z.write(os.path.join(root, file))  # Создание относительных путей и запись файлов в архив
             z.close()
-            print(spaces + "rename")
+            # print(spaces + "rename")
             os.rename(temp_path_zip, path_destination_zip)
 
