ad52781694a07b8b73e111b4645f47ba18eb7b23
misha <mike008@bk.ru>
2021-05-18 19:15:35 +0300

    +

--- find_install_tachiyomi-extensions/main.py
+++ find_install_tachiyomi-extensions/main.py
@@ -1,24 +1,15 @@
+import argparse
 import os
+import shutil as sh
 import subprocess
 import json
-
-path_git_temp_tach_ext = r"/home/misha/repos/tachiyomi-extensions"
-path_name_tach_ext = "/home/misha/PycharmProjects/Python/find_install_tachiyomi-extensions/_name_tachiyomi-extensions.txt"
-# appName : "Tachiyomi: $extName",
-prefixAppName = "Tachiyomi: "
-# setProperty("archivesBaseName", "tachiyomi-$pkgNameSuffix-v$versionName")
-prefixFileName = "tachiyomi-"
-
-example_app_name = "Tachiyomi: Bato.to  1.2.7"
 
 
 class Tree:
     def __init__(self):
         self.root = {}
-        self.all_str = []
 
     def add(self, s: str):
-        self.all_str.append(s)
         node = self.root
         for ch in s:
             if ch not in node:
@@ -31,34 +22,36 @@ class Tree:
             if ch not in node:
                 return False
             node = node[ch]
-        for i in range(len(self.all_str) - 1, -1, -1):
-            if s in self.all_str[i]:
-                self.all_str.pop(i)
         return True
 
 
-name_files = []
-os.chdir(path_git_temp_tach_ext)
-tree = Tree()
-with open(path_name_tach_ext) as file:
-    for line in file:
-        tree.add(line)
+def main(git_tach_ext: str, install_tach_ext: str, dest_copy_apk: str):
+    name_files = []
+    os.chdir(git_tach_ext)
+    tree = Tree()
+    with open(install_tach_ext) as file:
+        for line in file:
+            tree.add(line)
 
-subprocess.check_call(["git", "checkout", "repo"])
-file_path = "index.json"
-min_file_path = "index.min.json"
-with open(file_path, "r") as file:
-    data = json.load(file)
-    for object_json in data:
-        if "name" in object_json and tree.find(object_json['name']):
-            name_files.append(object_json['apk'])
+    subprocess.check_call(["git", "checkout", "repo"])
+    file_path = "index.json"
+    with open(file_path, "r") as file:
+        os.chdir('apk')
+        data = json.load(file)
+        for object_json in data:
+            if "name" in object_json and tree.find(object_json['name']):
+                if 'apk' in object_json and os.path.exists(object_json['apk']):
+                    sh.move(object_json['apk'], dest_copy_apk)
+                else:
+                    print("not found")
 
-os.chdir('apk')
-print("count files " + str(len(name_files)))
-for file in name_files:
-    if os.path.exists(file):
-        print('ok ' + file)
-    else:
-        print("no " + file)
-for i in tree.all_str:
-    print(i)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument("-gte", "--git_tach_ext", type=str, help="git dir tachiyomi extensions")
+    parser.add_argument("-da", "--dest_apk", type=str, help="dir ")
+    parser.add_argument("-nite", "--name_install_tach_ext", type=str,
+                        help="file with the names of installed extensions")
+    args = parser.parse_args()
+
+    main(args.git_tach_ext, args.name_install_tach_ext, args.name_install_tach_ext)
