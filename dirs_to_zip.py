50c85a9ac4d8218775e8d9d45303dea63c92d380
misha <mike008@bk.ru>
2021-07-11 23:45:29 +0300

    +

--- dirs_to_zip.py
+++ dirs_to_zip.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import shutil
 import zipfile
 
 # todo добавить логирование
@@ -8,6 +9,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='from dir dirs to zip')
     parser.add_argument("source_path_dir", type=str, help="source path dir")
     parser.add_argument("destination_path_dir", type=str, help="destination path dir")
+    parser.add_argument("-m", "--move", action='store_true', help="delete source")
     args = parser.parse_args()
 
     os.chdir(args.source_path_dir)
@@ -22,18 +24,18 @@ if __name__ == '__main__':
             zip_name = d.name + '.zip'
             path_destination_zip = os.path.join(destination, zip_name)
             if os.path.exists(path_destination_zip):
-                if os.path.exists(zip_name):
-                    os.remove(zip_name)
                 # print(spaces + "exist destination")
                 continue
-            temp_path_zip = os.path.join(destination, "temp.zip")
+            temp_path_zip = os.path.join(destination, path_destination_zip + ".temp")
             # print(spaces + "create zip")
             z = zipfile.ZipFile(temp_path_zip, 'w')  # Создание нового архива
             # print(spaces + "fill zip")
             for root, dirs, files in os.walk(d.path):  # Список всех файлов и папок в директории folder
                 for file in files:
-                    z.write(os.path.join(root, file))  # Создание относительных путей и запись файлов в архив
+                    full_path = os.path.join(root, file)
+                    z.write(full_path)  # Создание относительных путей и запись файлов в архив
             z.close()
             # print(spaces + "rename")
             os.rename(temp_path_zip, path_destination_zip)
-
+            if args.move:
+                shutil.rmtree(d)
