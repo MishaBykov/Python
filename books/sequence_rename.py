717849f443a0e2585b2448945afcffa60512037e
mikhail.bykov <mihail.b@sunrise-r.ru>
2023-03-18 16:18:31 +0300

    edit xml

--- books/sequence_rename.py
+++ books/sequence_rename.py
@@ -1,55 +1,34 @@
 # -*- coding: utf-8 -*-
 import argparse
 import os
-import xml.etree.ElementTree as ET
+import xml.etree.cElementTree as ET
+import time
 
-from rename.Renamer import Renamer
 
-
-def find_sequence(xml_file: str):
+def editXML(filename):
+    """
+    Редактируем XML файл.
+    """
+    ET.register_namespace('l', "http://www.w3.org/1999/xlink")
+    ET.register_namespace('', "http://www.gribuser.ru/xml/fictionbook/2.0")
     xpath = ".//{*}description/{*}title-info/{*}sequence"
-    try:
-        root = ET.parse(xml_file).getroot()
-    except ET.ParseError as e:
-        print('[ParseError\n' + str(e.msg) + "\n" + xml_file + ']')
-        return None
+
+    tree = ET.ElementTree(file=filename)
+    root = tree.getroot()
+
     sequence = root.find(xpath)
-    return sequence.attrib if sequence is not None else {'name': 'not sequence'}
 
+    sequence.attrib['name'] = 'Алхимик'
 
-def dir_root_books(dir_path: str):
-    entry: os.DirEntry
-    entries = list(os.scandir(dir_path))
-    sequence_files_fb2 = {}
-    for entry in entries:
-        if entry.is_dir():
-            dir_root_books(entry.path)
-        else:
-            ext = os.path.splitext(entry.name)[-1]
-            if ext == '.fb2':
-                sequence = find_sequence(entry.path)
-                if sequence is None:
-                    continue
-                if sequence["name"] in sequence_files_fb2:
-                    sequence_files_fb2[sequence["name"]].append((entry, sequence))
-                else:
-                    sequence_files_fb2[sequence["name"]] = [(entry, sequence)]
-
-    r = Renamer(dir_path)
-    for key in sequence_files_fb2:
-        for entry, sequence in sequence_files_fb2[key]:
-            number = (" " + sequence["number"]) if "number" in sequence else ""
-            r.rename(entry.name, sequence["name"] + number + ".fb2")
-    if len(sequence_files_fb2) == 1:
-        sequence_name = list(sequence_files_fb2.keys())[0]
-        if os.path.basename(dir_path) != sequence_name:
-            r = Renamer(os.path.dirname(dir_path))
-            r.rename(os.path.basename(dir_path), sequence_name)
+    tree = ET.ElementTree(root)
+    tree.write(filename, encoding='UTF-8', xml_declaration=True)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='management books')
-    parser.add_argument("-drp", "--dit_root_path", type=str, help="input path")
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser(description='management books')
+    # parser.add_argument("-drp", "--dit_root_path", type=str, help="input path")
+    # args = parser.parse_args()
 
-    dir_root_books(args.dit_root_path)
+    xml_file_path = '/home/misha/temp/Алхимик/Алхимик 1.fb2'
+
+    editXML(xml_file_path)
