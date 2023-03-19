b2a99c189648ee30e2337ba854523e5b224998a0
mikhail.bykov <mihail.b@sunrise-r.ru>
2023-03-19 16:26:02 +0300

    update sequence as folder name

--- books/sequence_rename.py
+++ books/sequence_rename.py
@@ -1,34 +1,40 @@
 # -*- coding: utf-8 -*-
 import argparse
-import os
+import os.path
 import xml.etree.cElementTree as ET
-import time
 
 
-def editXML(filename):
-    """
-    Редактируем XML файл.
-    """
+def update_sequence_name_fb2(filename: str, new_sequence_name: str):
     ET.register_namespace('l', "http://www.w3.org/1999/xlink")
     ET.register_namespace('', "http://www.gribuser.ru/xml/fictionbook/2.0")
     xpath = ".//{*}description/{*}title-info/{*}sequence"
 
     tree = ET.ElementTree(file=filename)
     root = tree.getroot()
-
     sequence = root.find(xpath)
-
-    sequence.attrib['name'] = 'Алхимик'
+    if sequence is None:
+        print(filename + " -> not found sequence")
+        return
+    sequence.attrib['name'] = new_sequence_name
 
     tree = ET.ElementTree(root)
     tree.write(filename, encoding='UTF-8', xml_declaration=True)
 
 
 if __name__ == "__main__":
-    # parser = argparse.ArgumentParser(description='management books')
-    # parser.add_argument("-drp", "--dit_root_path", type=str, help="input path")
-    # args = parser.parse_args()
+    parser = argparse.ArgumentParser(description=
+                                     'in the folder with fb2 files update the name of the series as the folder name')
+    parser.add_argument("-dp", "--dir_path", type=str, help="directory path")
+    args = parser.parse_args()
 
-    xml_file_path = '/home/misha/temp/Алхимик/Алхимик 1.fb2'
+    dir_path = args.dir_path
+    dir_name = os.path.basename(dir_path)
 
-    editXML(xml_file_path)
+    if not os.path.exists(dir_path):
+        print("dir not exist")
+        exit()
+    for name in os.listdir(dir_path):
+        if os.path.splitext(name)[1] != ".fb2":
+            print(name + ': no .fb2')
+            continue
+        update_sequence_name_fb2(os.path.join(dir_path, name), dir_name)
