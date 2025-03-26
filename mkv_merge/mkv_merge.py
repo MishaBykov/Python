fe2710e17ad367a71524345fd389379b57ee854d
misha <stun007@gmail.com>
2025-03-26 10:31:16 +0300

    add args name

--- mkv_merge/mkv_merge.py
+++ mkv_merge/mkv_merge.py
@@ -6,11 +6,11 @@ import subprocess
 def parse_args():
     parser = argparse.ArgumentParser(
         description='Merge mkv video and audio, and subs. Audio and subs Contain the prefix name of the video file')
-    parser.add_argument("dir_ffmpeg", type=str, help="directory ffmpeg")
-    parser.add_argument("video_input", type=str, help="directory video")
-    parser.add_argument("audio_input", type=str, help="directory audio")
-    parser.add_argument("dir_output", type=str, help="directory output")
+    parser.add_argument("--dir_ffmpeg", type=str, help="directory ffmpeg")
+    parser.add_argument("--video_input", type=str, help="directory video")
+    parser.add_argument("--audio_input", type=str, help="directory audio")
     parser.add_argument("--subs_input", type=str, help="directory subs", required=False)
+    parser.add_argument("--dir_output", type=str, help="directory output")
     parser.add_argument("--ffmpeg_prefix", type=str, help="prefix ffmpeg executable file", default="ffmpeg",
                         required=False)
     return parser.parse_args()
